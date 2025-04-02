136f8a9286fddb6f1fde23c9b70c9e83658e2998
David Otterdahl <david.otterdahl@gmail.com>
2025-04-02 11:31:44 +0200

    Treat values as case insensitive when loading from SXL

--- RSMPCommon/RSMPGS_ProcessImage.cs
+++ RSMPCommon/RSMPGS_ProcessImage.cs
@@ -36,8 +36,8 @@ namespace nsRSMPGS
     public List<cSiteIdObject> SiteIdObjects = new List<cSiteIdObject>();
 
     // These ones are flat
-    public Dictionary<string, cRoadSideObject> RoadSideObjects = new Dictionary<string, cRoadSideObject>(StringComparer.InvariantCultureIgnoreCase);
-    public Dictionary<string, cValueTypeObject> ValueTypeObjects = new Dictionary<string, cValueTypeObject>(StringComparer.InvariantCultureIgnoreCase);
+    public Dictionary<string, cRoadSideObject> RoadSideObjects = new Dictionary<string, cRoadSideObject>(StringComparer.InvariantCulture);
+    public Dictionary<string, cValueTypeObject> ValueTypeObjects = new Dictionary<string, cValueTypeObject>(StringComparer.InvariantCulture);
 
     public List<cAlarmObject> AlarmObjects = new List<cAlarmObject>();
     public List<cCommandObject> CommandObjects = new List<cCommandObject>();
