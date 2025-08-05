rsmp-nordic/rsmp_simulator

commit 2104895f695fd748ac5bccfc1865a7b7514f8361

Use json object in CommandResponse

RSMPGS1/RSMPGS1_JSon.cs
@@ -609,7 +609,7 @@ namespace nsRSMPGS
               rv.age = "unknown";
 
               // MessageNotAck
-              sError = "Value and/or type is out of range or invalid for this RSMP protocol version, type: " + CommandReturnValue.Value.GetValueType() + ", value: " + ((CommandRequest_Value.v.Length < 10) ? CommandRequest_Value.v : CommandRequest_Value.v.Substring(0, 9) + "...");
+              sError = "Value and/or type is out of range or invalid for this RSMP protocol version, type: " + CommandReturnValue.Value.GetValueType() + ", value: " + ((CommandRequest_Value.v.ToString().Length < 10) ? CommandRequest_Value.v : CommandRequest_Value.v.ToString().Substring(0, 9) + "...");
               RSMPGS.SysLog.SysLog(cSysLogAndDebug.Severity.Error, "{0}", sError);
               if (!bHasSentAckOrNack)
               {
@@ -622,7 +622,7 @@ namespace nsRSMPGS
             {
               if (RSMPGS.MainForm.ToolStripMenuItem_StoreBase64Updates.Checked)
               {
-                CommandReturnValue.Value.SetValue(RSMPGS.SysLog.StoreBase64DebugData(CommandRequest_Value.v));
+                CommandReturnValue.Value.SetValue(RSMPGS.SysLog.StoreBase64DebugData(CommandRequest_Value.v.ToString()));
               }
             }
             else

RSMPCommon/RSMPGS_JSon.cs
@@ -1357,18 +1357,18 @@ namespace nsRSMPGS
       return "[" + objectsString + "]";
     }
 
-    public bool ValidateTypeAndRange(string sType, string sValue, Dictionary<string, string> sEnums)
+    public bool ValidateTypeAndRange(string sType, object oValue, Dictionary<string, string> sEnums)
     {
-      return ValidateTypeAndRange(sType, sValue, sEnums, 0, 0);
+      return ValidateTypeAndRange(sType, oValue, sEnums, 0, 0);
     }
 
-    public bool ValidateTypeAndRange(string sType, string sValue, Dictionary<string, string> sEnums, double dMin, double dMax)
+    public bool ValidateTypeAndRange(string sType, object oValue, Dictionary<string, string> sEnums, double dMin, double dMax)
     {
       bool bUseCaseSensitiveValue = cHelper.IsSettingChecked("UseCaseSensitiveValue");
       var comparisonType = StringComparison.Ordinal;
       if (!bUseCaseSensitiveValue) { comparisonType = StringComparison.OrdinalIgnoreCase; }
 
-      if (sValue == null)
+      if (oValue == null)
       {
         return false;
       }
@@ -1386,7 +1386,7 @@ namespace nsRSMPGS
         case "integer":
           try
           {
-            Int32 iValue = Int32.Parse(sValue);
+            Int32 iValue = Int32.Parse(oValue.ToString());
             bValueIsValid = true;
           }
           catch { }
@@ -1395,7 +1395,7 @@ namespace nsRSMPGS
         case "long":
           try
           {
-            Int32 iValue = Int32.Parse(sValue);
+            Int32 iValue = Int32.Parse(oValue.ToString());
             bValueIsValid = true;
           }
           catch { }
@@ -1405,7 +1405,7 @@ namespace nsRSMPGS
         case "real":
           try
           {
-            Double dValue = Double.Parse(sValue);
+            Double dValue = Double.Parse(oValue.ToString());
             bValueIsValid = true;
           }
           catch { }
@@ -1415,10 +1415,11 @@ namespace nsRSMPGS
           // Boolean is treated as an enum in Excel/CSV, but not in YAML. To
           // preserve backwards compability we need to treat this as case
           // insensitive for now
-          bValueIsValid = sValue.Equals("true", StringComparison.OrdinalIgnoreCase) ||
-            sValue.Equals("false", StringComparison.OrdinalIgnoreCase) ||
-            sValue.Equals("0", StringComparison.OrdinalIgnoreCase) ||
-            sValue.Equals("1", StringComparison.OrdinalIgnoreCase);
+          
+          bValueIsValid = oValue.ToString().Equals("true", StringComparison.OrdinalIgnoreCase) ||
+            oValue.ToString().Equals("false", StringComparison.OrdinalIgnoreCase) ||
+            oValue.ToString().Equals("0", StringComparison.OrdinalIgnoreCase) ||
+            oValue.ToString().Equals("1", StringComparison.OrdinalIgnoreCase);
           break;
 
         case "base64":
@@ -1426,7 +1427,7 @@ namespace nsRSMPGS
           {
             Encoding encoding;
             encoding = Encoding.GetEncoding("IBM437");
-            byte[] Base64Bytes = encoding.GetBytes(sValue);
+            byte[] Base64Bytes = encoding.GetBytes(oValue.ToString());
             char[] Base64Chars = encoding.GetChars(Base64Bytes);
             byte[] Base8Bytes = System.Convert.FromBase64CharArray(Base64Chars, 0, Base64Chars.GetLength(0));
             bValueIsValid = true;
@@ -1440,7 +1441,7 @@ namespace nsRSMPGS
           {
             try
             {
-              UInt32 iValue = UInt32.Parse(sValue);
+              UInt32 iValue = UInt32.Parse(oValue.ToString());
               bValueIsValid = true;
             }
             catch { }
@@ -1470,7 +1471,7 @@ namespace nsRSMPGS
 
             try
             {
-              Double dValue = Double.Parse(sValue);
+              Double dValue = Double.Parse(oValue.ToString());
               bValueIsValid = dValue <= dMax && dValue >= dMin;
 
               if (sEnums != null && sEnums.Count > 0)
@@ -1478,7 +1479,7 @@ namespace nsRSMPGS
                 bValueIsValid = false;
                 foreach (string sEnum in sEnums.Keys)
                 {
-                  if (sValue.Equals(sEnum, comparisonType))
+                  if (oValue.ToString().Equals(sEnum, comparisonType))
                   {
                     bValueIsValid = true;
                   }
@@ -1497,7 +1498,7 @@ namespace nsRSMPGS
                 bValueIsValid = false;
                 foreach (string sEnum in sEnums.Keys)
                 {
-                  if (sValue.Equals(sEnum, comparisonType))
+                  if (oValue.ToString().Equals(sEnum, comparisonType))
                   {
                     bValueIsValid = true;
                   }

RSMPCommon/RSMPGS_Messages.cs
@@ -161,7 +161,7 @@ namespace RSMP_Messages
   {
     public string cCI;  // CommandCodeId
     public string n;  // Name
-    public string v;  // Value
+    public object v;  // Value
     public string age;  // Age
   }
 

RSMPGS2/RSMPGS2_JSon.cs
@@ -425,7 +425,7 @@ namespace nsRSMPGS
             }
             else
             {
-              sStatusValue = (Reply.v.Length < 10) ? Reply.v : Reply.v.Substring(0, 9) + "...";
+              sStatusValue = (Reply.v.ToString().Length < 10) ? Reply.v.ToString() : Reply.v.ToString().Substring(0, 9) + "...";
             }
             sError = "Value and/or type is out of range or invalid for this RSMP protocol version, type: " + CommandReturnValue.Value.GetValueType() + ", value: " + sStatusValue;
             RSMPGS.SysLog.SysLog(cSysLogAndDebug.Severity.Error, sError);
@@ -442,7 +442,7 @@ namespace nsRSMPGS
           {
             if (RSMPGS.MainForm.ToolStripMenuItem_StoreBase64Updates.Checked)
             {
-              RSMPGS.SysLog.StoreBase64DebugData(Reply.v);
+              RSMPGS.SysLog.StoreBase64DebugData(Reply.v.ToString());
             }
             CommandEvent.oValue = "base64";
           }
@@ -451,7 +451,7 @@ namespace nsRSMPGS
             CommandEvent.oValue = Reply.v;
           }
           CommandEvent.sAge = Reply.age;
-          CommandReturnValue.sLastRecValue = Reply.v;
+          CommandReturnValue.sLastRecValue = Reply.v.ToString();
           CommandReturnValue.sLastRecAge = Reply.age;
 
           if (RSMPGS_Main.bWriteEventsContinous)

commit 3383e1dc3e69b901b5087dbc8749167710b9ceb5

Fix incorrect merge
Fix changes introduced with 2104895f695fd748ac5bccfc1865a7b7514f8361

RSMPGS1/RSMPGS1_JSon.cs
@@ -545,7 +545,7 @@ namespace nsRSMPGS
             CommandReturnValue.Value.GetValueMin(), CommandReturnValue.Value.GetValueMax()))
           {
             // MessageNotAck
-            sError = "Value and/or type is out of range or invalid for this RSMP protocol version, type: " + CommandReturnValue.Value.GetValueType() + ", value: " + ((CommandRequest_Value.v.Length < 10) ? CommandRequest_Value.v : CommandRequest_Value.v.Substring(0, 9) + "...");
+            sError = "Value and/or type is out of range or invalid for this RSMP protocol version, type: " + CommandReturnValue.Value.GetValueType() + ", value: " + ((CommandRequest_Value.v.ToString().Length < 10) ? CommandRequest_Value.v : CommandRequest_Value.v.ToString().Substring(0, 9) + "...");
             RSMPGS.SysLog.SysLog(cSysLogAndDebug.Severity.Error, "{0}", sError);
             if (!bHasSentAckOrNack)
             {