50ea0f90ee04971c3f764b4be87680eecbacbe76
David Otterdahl <david@otterdahl.org>
2026-05-16 16:48:55 +0200

    rsVersion.SXL might be null in response (2)

--- RSMPCommon/RSMPGS_JSon.cs
+++ RSMPCommon/RSMPGS_JSon.cs
@@ -875,7 +875,7 @@ namespace nsRSMPGS
             RSMPGS.SysLog.SysLog(cSysLogAndDebug.Severity.Warning, "Client RSMP version '{0}' is unknown", Version_RSMP.vers.Trim());
           }
         }
-        if(rsVersion != null)
+        if(rsVersion.SXL != null)
           bSXLVersionIsOk = (rsVersion.SXL.Trim() == RSMPGS.MainForm.textBox_SignalExchangeListVersion.Text.Trim());
 
 #if _RSMPGS1
